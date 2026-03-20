97527bd9c184aae6e0d3bb8bd4be8816b3186623
asrinordek <asrinordek@gmail.com>
2026-03-20 17:27:43 +0100

    in bearbeitung

--- Python/main.py
+++ Python/main.py
@@ -13,6 +13,7 @@ Vocis = {
     'aimer': 'mögen'
 }
 count = 0
+fehler = [] #Liste für Falsche Antworten
 
 for franz in Vocis:
     print('Übersetze dieses Wort: ')
@@ -27,8 +28,26 @@ for franz in Vocis:
     else: 
         print("Falsch ")
         print("Richtig wäre:", Vocis[franz])
+        fehler.append(franz) #Falsche Wörter werden gespeichert
+
+#fehler wiederholen
+print ('\---Fehler wiederholen---')
+
+for franz in fehler:
+    print('Übersetze dieses Wort:')
+    print(franz)
+
+    antwort = input('Deutsch')
+
+    if antwort == Vocis[franz]
+    print ('jetzt Richtig!')
+    count +=  1
+
+else   
 
 
 
 print('Du hast', count, 'von', len(Vocis), 'richtig')
+
+
 
